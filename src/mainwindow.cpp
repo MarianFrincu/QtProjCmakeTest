9f587d2055c2aa9a0fd704a5fc681ed97b1716da
Marian <marianxd2345@gmail.com>
2025-09-12 18:24:37 +0300

    Add project files

---
+++ src/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPushButton>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    QPushButton* button = new QPushButton("MIAU", this);
+    button->setFixedSize(100, 50);
+    button->move(300, 300);
+
+    connect(button, &QPushButton::clicked, this, [button](){
+        button->setText("HAM");
+    });
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
